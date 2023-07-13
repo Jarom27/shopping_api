961abe161dea29ad51af484f40b03924bd79a23c
Jarom Mariscal <jaromero_marz234@outlook.com>
2023-07-13 11:03:14 -0700

    Añadir category seeder al commit pasado

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Item;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,10 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         //
-        Category::factory(5)->create();
+        Category::factory()->has(Item::factory()->count(6))->create();
+        Category::factory()->has(Item::factory()->count(4))->create();
+        Category::factory()->has(Item::factory()->count(7))->create();
+        Category::factory()->has(Item::factory()->count(3))->create();
+        Category::factory()->has(Item::factory()->count(8))->create();
     }
 }
